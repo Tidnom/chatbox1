4b5dd18d1ed43641d8348fbe0875e4417d6d5daf
Tidnom <115793248+Tidnom@users.noreply.github.com>
2024-01-21 20:11:36 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,33 +1,40 @@
 from dotenv import load_dotenv
-load_dotenv
+load_dotenv() ## loading all the environment variables
 
+import streamlit as st
 import os
 import google.generativeai as genai
-import streamlit as st
+
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-model=genai.GenerativeModel("gemini-pro")
-chat=model.start_chat(history=[])
+
+## function to load Gemini Pro model and get repsonses
+model=genai.GenerativeModel("gemini-pro") 
+chat = model.start_chat(history=[])
 
 def get_gemini_response(question):
     response=chat.send_message(question,stream=True)
     return response
 
-st.set_page_config("Q&A Chatbot")
-st.header("Conversational Chatbot")
-if 'chat history' not in st.session_state:
-    st.session_state['chat_history']=[]
+##initialize our streamlit app
+st.set_page_config(page_title="Q&A App")
+st.header("Conversational ChatBot")
+
+# Initialize session state for chat history if it doesn't exist
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
 
 input=st.text_input("Input: ",key="input")
 submit=st.button("Send")
 
-if input and submit:
+if submit and input:
     response=get_gemini_response(input)
-    st.session_state['chat_history'].append(("You",input))
-    st.subheader("Response: ")
+    # Add user query and response to session state chat history
+    st.session_state['chat_history'].append(("You", input))
+    st.subheader("The Response is")
     for chunk in response:
         st.write(chunk.text)
-        st.session_state['chat_history'].append(("Response",chunk.text))
-
-st.subheader("Chat history: ")
-for role, response in st.session_state['chat_history']:
-    st.write(f"{role}: {response}")
+        st.session_state['chat_history'].append(("Bot", chunk.text))
+st.subheader("Chat History:")
+    
+for role, text in st.session_state['chat_history']:
+    st.write(f"{role}: {text}")
